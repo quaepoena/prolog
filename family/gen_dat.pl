3ec382625cfca094629dabb9b77bad1dccee0ee8
QP <qp@example.com>
2021-10-24 18:15:18 +0200

    initial add of gen_dat.pl

---
+++ family/gen_dat.pl
@@ -0,0 +1,16 @@
+% Genealogical database.
+
+male(tom).
+male(chris).
+male(ben).
+male(arthur).
+male(greg).
+
+female(polly).
+female(patty).
+
+parent(arthur, chris).
+parent(polly, tom).
+parent(polly, ben).
+parent(greg, polly).
+parent(patty, polly).
