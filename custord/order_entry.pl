5a0483f70c3a4d4b8010f3ad01d7497cc73d4cb2
QP <qp@example.com>
2021-10-24 18:20:06 +0200

    initial add of customer order entry program

---
+++ custord/order_entry.pl
@@ -0,0 +1,13 @@
+% Customer order entry program.
+
+customer(joe, pittsburgh, aaa).
+customer(jane, 'new york', bbb).
+customer(john, 'san francisco', ccc).
+
+sale(0001, chair, 2).
+sale(0002, table, 5).
+sale(0003, desk, 3).
+
+inventory_record(0001, 5).
+inventory_record(0002, 2).
+inventory_record(0003, 3).
