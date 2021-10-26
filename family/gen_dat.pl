37ca4f776514716e6cc4cfd3900c520cdb551474
QP <qp@example.com>
2021-10-26 09:12:46 +0200

    adding various predicates, currently buggy

--- family/gen_dat.pl
+++ family/gen_dat.pl
@@ -4,11 +4,14 @@ male(tom).
 male(chris).
 male(ben).
 male(arthur).
+male(john).
+male(randy).
 male(greg).
 male(lyle).
 male(robert).
 
 female(polly).
+female(sally).
 female(patty).
 female(wendy).
 female(mary).
@@ -18,9 +21,15 @@ parent(polly, tom).
 parent(polly, ben).
 parent(greg, polly).
 parent(patty, polly).
+parent(greg, sally).
+parent(patty, sally).
+parent(greg, john).
+parent(patty, john).
+parent(greg, randy).
+parent(patty, randy).
 parent(wendy, patty).
 parent(lyle, patty).
-parent(mary, grep).
+parent(mary, greg).
 parent(robert, greg).
 
 daughter(D, P) :-
@@ -58,14 +67,58 @@ great_grandmother(GGM, C) :-
     parent(GGM, GP),
     female(GGM).
 
-great_grandmother(GGM, C) :-
-    parent(P, C),
-    parent(GP, P),
-    parent(GGM, GP),
-    female(GGM).
-
 great_grandfather(GGF, C) :-
     parent(P, C),
     parent(GP, P),
     parent(GGF, GP),
     male(GGF).
+
+sibling(X, Y) :-
+    parent(P, X),
+    parent(P, Y),
+    X \= Y.
+
+% TODO: This is buggy.
+brother(X, Y) :-
+    parent(P, X),
+    parent(P, Y),
+    male(X),
+    X \= Y.
+
+sister(X, Y) :-
+    parent(P, X),
+    parent(P, Y),
+    female(X),
+    X \= Y.
+
+% TODO: This needs to be fixed.
+brothers(X, Y) :-
+    brother(X, Y),
+    brother(Y, X).
+    %% parent(P, X),
+    %% parent(P, Y),
+    %% male(X),
+    %% male(Y),
+    %% X \= Y.
+
+sisterst(X, Y) :-
+    parent(P, X),
+    parent(P, Y),
+    female(X),
+    female(Y),
+    X \= Y.
+
+aunt(A, N) :-
+    parent(P, N),
+    sibling(A, P),
+    female(A).
+
+uncle(U, N) :-
+    parent(P, N),
+    sibling(U, P),
+    male(U).
+
+cousin(C, X) :-
+    parent(P, X),
+    sibling(S, P),
+    parent(S, C).
