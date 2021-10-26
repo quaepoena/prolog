d0efcaa39292aae2edee5b7fe8f9bb53e2cba93c
QP <qp@example.com>
2021-10-26 06:39:06 +0200

    adding various predicates

--- family/gen_dat.pl
+++ family/gen_dat.pl
@@ -5,15 +5,34 @@ male(chris).
 male(ben).
 male(arthur).
 male(greg).
+male(lyle).
+male(robert).
 
 female(polly).
 female(patty).
+female(wendy).
+female(mary).
 
 parent(arthur, chris).
 parent(polly, tom).
 parent(polly, ben).
 parent(greg, polly).
 parent(patty, polly).
+parent(wendy, patty).
+parent(lyle, patty).
+parent(mary, grep).
+parent(robert, greg).
+
+daughter(D, P) :-
+    parent(P, D),
+    female(D).
+
+son(S, P) :-
+    parent(P, S),
+    male(S).
+
+child(C, P) :-
+    parent(P, C).
 
 mother(M, C) :-
     parent(M, C),
@@ -22,3 +41,31 @@ mother(M, C) :-
 father(F, C) :-
     parent(F, C),
     male(F).
+
+grandmother(GM, C) :-
+    parent(P, C),
+    parent(GM, P),
+    female(GM).
+
+grandfather(GF, C).
+    parent(P, C),
+    parent(GF, P),
+    male(GF).
+
+great_grandmother(GGM, C) :-
+    parent(P, C),
+    parent(GP, P),
+    parent(GGM, GP),
+    female(GGM).
+
+great_grandmother(GGM, C) :-
+    parent(P, C),
+    parent(GP, P),
+    parent(GGM, GP),
+    female(GGM).
+
+great_grandfather(GGF, C) :-
+    parent(P, C),
+    parent(GP, P),
+    parent(GGF, GP),
+    male(GGF).
